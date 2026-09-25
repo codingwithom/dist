f34d3c6e07cdd8de54ad055a0ff380d8d5f5faad
justgit-om <128235069+justgit-om@users.noreply.github.com>
2026-09-25 05:38:04 +0000

    build: update static assets [2026-09-25T05:38:04.437Z]

---
+++ assets/list-tree-BI_IuHvu.js
@@ -0,0 +1,31 @@
+import { c as createLucideIcon } from "./index-Djut9CSo.js";
+const __iconNode$2 = [
+  [
+    "path",
+    {
+      d: "M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z",
+      key: "1kt360"
+    }
+  ]
+];
+const Folder = createLucideIcon("folder", __iconNode$2);
+const __iconNode$1 = [
+  ["rect", { width: "7", height: "7", x: "3", y: "3", rx: "1", key: "1g98yp" }],
+  ["rect", { width: "7", height: "7", x: "14", y: "3", rx: "1", key: "6d4xhi" }],
+  ["rect", { width: "7", height: "7", x: "14", y: "14", rx: "1", key: "nxv5o0" }],
+  ["rect", { width: "7", height: "7", x: "3", y: "14", rx: "1", key: "1bb6yr" }]
+];
+const LayoutGrid = createLucideIcon("layout-grid", __iconNode$1);
+const __iconNode = [
+  ["path", { d: "M8 5h13", key: "1pao27" }],
+  ["path", { d: "M13 12h8", key: "h98zly" }],
+  ["path", { d: "M13 19h8", key: "c3s6r1" }],
+  ["path", { d: "M3 10a2 2 0 0 0 2 2h3", key: "1npucw" }],
+  ["path", { d: "M3 5v12a2 2 0 0 0 2 2h3", key: "x1gjn2" }]
+];
+const ListTree = createLucideIcon("list-tree", __iconNode);
+export {
+  Folder as F,
+  LayoutGrid as L,
+  ListTree as a
+};
