759bb060b0523abccdf3c5a6e001ac29dab89dc9
justgit-om <128235069+justgit-om@users.noreply.github.com>
2026-09-25 13:31:02 +0000

    build: update static assets [2026-09-25T13:31:02.222Z]

---
+++ assets/copy-DoD1_LDe.js
@@ -0,0 +1,15 @@
+import { c as createLucideIcon } from "./index-HDO9hi2b.js";
+const __iconNode$1 = [
+  ["path", { d: "M21.801 10A10 10 0 1 1 17 3.335", key: "yps3ct" }],
+  ["path", { d: "m9 11 3 3L22 4", key: "1pflzl" }]
+];
+const CircleCheckBig = createLucideIcon("circle-check-big", __iconNode$1);
+const __iconNode = [
+  ["rect", { width: "14", height: "14", x: "8", y: "8", rx: "2", ry: "2", key: "17jyea" }],
+  ["path", { d: "M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2", key: "zix9uf" }]
+];
+const Copy = createLucideIcon("copy", __iconNode);
+export {
+  CircleCheckBig as C,
+  Copy as a
+};
