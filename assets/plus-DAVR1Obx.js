01b31cf1aa33045660b3e1a4c162de2926b5a06b
justgit-om <128235069+justgit-om@users.noreply.github.com>
2026-09-26 08:50:26 +0000

    Deploy fixed PW chapter lecture synchronization and strict reCAPTCHA

---
+++ assets/plus-DAVR1Obx.js
@@ -0,0 +1,21 @@
+import { c as createLucideIcon } from "./index-CSKiqNtJ.js";
+const __iconNode$1 = [
+  [
+    "path",
+    {
+      d: "M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",
+      key: "1a8usu"
+    }
+  ],
+  ["path", { d: "m15 5 4 4", key: "1mk7zo" }]
+];
+const Pencil = createLucideIcon("pencil", __iconNode$1);
+const __iconNode = [
+  ["path", { d: "M5 12h14", key: "1ays0h" }],
+  ["path", { d: "M12 5v14", key: "s699le" }]
+];
+const Plus = createLucideIcon("plus", __iconNode);
+export {
+  Plus as P,
+  Pencil as a
+};
